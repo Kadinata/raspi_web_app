eafb890b31ba75ac6215a614e12ac5b020eff9b7
Kadinata <2396847+Kadinata@users.noreply.github.com>
2023-11-12 23:02:02 -0600

    Adding documentation to endpoint request module files and refactoring the endpoint request custom hooks.

--- src/modules/endpoint_request/EndpointRequest.js
+++ src/modules/endpoint_request/EndpointRequest.js
@@ -1,11 +1,24 @@
-//===========================================================================
-//  
-//===========================================================================
-const normalizePath = (path) => (`/${path}`);
+/**
+ * Endpoint Request: HTTP request module.
+ *
+ * This module provides functions to create and send HTTP requests to the server.
+ */
 
+/**
+ * @private Internal helper function to ensure endpoint URLs are prefixed with '/'
+ * @param {string} endpoint - Endpoint URL to normalize.
+ * @returns {string} - The provided endpoint URL prefixed with '/'
+ */
+const normalizeEndpoint = (endpoint) => (`/${endpoint}`);
+
+/**
+ * Send an HTTP GET request to the server at the provided endpoint.
+ * @param {string} endpoint - API endpoint URL to send the GET request to.
+ * @returns {Promise} - A resolved promise containing the response, or a rejected promise if an error is encountered.
+ */
 const get = async (endpoint) => {
   try {
-    const response = await fetch(normalizePath(endpoint), {
+    const response = await fetch(normalizeEndpoint(endpoint), {
       method: 'get',
       credentials: 'include',
     });
@@ -16,11 +29,16 @@ const get = async (endpoint) => {
   }
 };
 
+/**
+ * Subscribe to a server-sent event (SSE) endpoint.
+ * @param {string} endpoint - Endpoint URL of the server-sent event stream.
+ * @param {function} onMessage - Message handler function to be invoked whenever a new message is sent from the server.
+ * @returns {EventSource} - An event source object associated with the SSE stream.
+ */
 const subscribe = (endpoint, onMessage) => {
-  console.log(`subscribing to: ${normalizePath(endpoint)}`)
-  const eventSource = new EventSource(normalizePath(endpoint));
+  console.log(`subscribing to: ${normalizeEndpoint(endpoint)}`)
+  const eventSource = new EventSource(normalizeEndpoint(endpoint));
   eventSource.onmessage = (event) => {
-    // console.log('onMessage:', {onMessage, event});
     if (typeof onMessage !== 'function') return;
     const eventData = JSON.parse(event.data);
     onMessage(eventData);
@@ -28,9 +46,15 @@ const subscribe = (endpoint, onMessage) => {
   return eventSource;
 };
 
+/**
+ * Send an HTTP POST request to the server at the provided endpoint.
+ * @param {string} endpoint - API endpoint URL to send the POST request to.
+ * @param {any} data - Any data to be sent to the server as the request's body. 
+ * @returns {Promise} - A resolved promise containing the response, or a rejected promise if an error is encountered.
+ */
 const post = async (endpoint, data) => {
   try {
-    const response = await fetch(normalizePath(endpoint), {
+    const response = await fetch(normalizeEndpoint(endpoint), {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
@@ -45,6 +69,7 @@ const post = async (endpoint, data) => {
   }
 };
 
+/** Bundle all of the methods into a single object */
 const EndpointRequest = {
   get,
   post,
--- src/modules/endpoint_request/hooks.js
+++ src/modules/endpoint_request/hooks.js
@@ -1,9 +1,16 @@
-//===========================================================================
-//  
-//===========================================================================
+/**
+ * Endpoint Request Hooks: Custom React hooks to simplify state management while making HTTP requests.
+ *
+ * This module provides custom hooks to manage component state while making HTTP requests.
+ */
 import React from 'react';
 import Endpoint from './EndpointRequest';
 
+/**
+ * @private Internal helper function to combine the responses from multiple GET requests into a single object.
+ * @param {object} endpoints - Endpoint URLs to request from and have their responses combined into a single object.
+ * @returns {object} - Combined responses from all of the requests to the provided endpoints.
+ */
 const serializeRequests = async ({ ...endpoints }) => {
   const result = {};
   for (const key in endpoints) {
@@ -14,56 +21,113 @@ const serializeRequests = async ({ ...endpoints }) => {
   return result;
 };
 
-const updateDataState = (prevData, newData) => {
-  const data = { ...prevData, ...newData };
-  return { data };
+/**
+ * @private Internal function to retrieve the initial request state.
+ * The initial state is wrapped in a function to prevent it from being mutated.
+ * @returns {object} - Initial request state containing fields for data, timestamp,
+ * error, and request completion flag.
+ */
+const getInitialRequestState = (initialData = {}) => {
+  const data = initialData;
+  const error = null;
+  const timestamp = Date.now();
+  const completed = false;
+  return { data, timestamp, error, completed };
 };
 
+/**
+ * @private Internal custom hook to manage request state.
+ * @returns {object} - An object containing the request state and functions
+ * to set new data, new error, or reset the request completion flag.
+ */
+const useRequestState = (initialData) => {
+  const [requestState, setRequestState] = React.useState(() => getInitialRequestState(initialData));
+
+  /** Function to update the data of the request state */
+  const setData = React.useCallback((newData) => {
+    setRequestState((prevState) => {
+      const timestamp = Date.now();
+      const completed = true;
+      const error = null;
+      const data = { ...prevState.data, ...newData };
+      return ({ ...prevState, data, error, timestamp, completed });
+    });
+  }, [setRequestState]);
+
+  /** Function to add an error to the request state */
+  const setError = React.useCallback((error) => {
+    const timestamp = Date.now();
+    const completed = true;
+    setRequestState((prevState) => ({ ...prevState, error, timestamp, completed }));
+  }, [setRequestState]);
+
+  /** Function to reset the request completion flag */
+  const resetCompletionFlag = React.useCallback(() => {
+    const completed = false;
+    setRequestState((prevState) => ({ ...prevState, completed }));
+  }, [setRequestState]);
+
+  return { requestState, setData, resetCompletionFlag, setError };
+};
+
+/**
+ * Custom hook to manage the component state while making one or more GET requests.
+ * @param {object} endpoints - Enpoint URLs to sent the GET requests to.
+ * @returns {object} - An object containing the state of the requests.
+ * @return {boolean} - result.completed: a flag that is set to true when the request
+ * completes whether or not the request results in an error. A false value indicates
+ * the server has not yet responded to the request. This flag can be used to determine
+ * whether or not to render the loading component.
+ * @return {number} - result.timestamp: Unix epoch indicating the time when the request completes.
+ * @return {error} - result.error: null if the request completes without error, otherwise this will
+ * contain the error from the request.
+ * @return {object} - result.data: The data contained in the response from the server.
+ */
 export const useDataRequest = (endpoints) => {
 
-  const [dataState, setDataState] = React.useState({data: {}});
-  const [completed, setCompleted] = React.useState(false);
-  const [timestamp, setTimestamp] = React.useState(Date.now());
-  const [error, setError] = React.useState(null);
+  const { requestState, setData, resetCompletionFlag, setError } = useRequestState();
 
   const fetchResource = React.useCallback(
     async () => {
-      setCompleted(false);
+      resetCompletionFlag();
       try {
         const newData = await serializeRequests(endpoints);
-        setDataState(({ data }) => updateDataState(data, newData));
+        setData(newData);
       } catch (err) {
         setError(err);
       }
-      setTimestamp(Date.now());
-      setCompleted(true);
     },
-    [setDataState, setCompleted, setError, endpoints]
+    [resetCompletionFlag, setData, setError, endpoints]
   );
 
   React.useEffect(() => {
     fetchResource();
   }, [fetchResource]);
 
-  return { ...dataState, timestamp, completed, error };
+  return { ...requestState };
 };
 
+/**
+ * Custom hook to manage the component state while subscribing to a server-sent event stream endpoint.
+ * @param {string} endpoint - Endpoint URL pointing to the server-sent event stream to subscribe to.
+ * @param {boolean} enable - A flag to enable or disable the subscription.
+ * @param {object} initialData - Optional data to initialize the hook's internal data state.
+ * @returns {object} - An object containing timestamp and data that are updated whenever a new message
+ * is received from the server.
+ */
 export const useStreamRequest = (endpoint, enable, initialData = {}) => {
 
-  const [dataState, setDataState] = React.useState({data: initialData});
-  const [timestamp, setTimestamp] = React.useState(Date.now());
+  const { requestState, setData } = useRequestState(initialData);
 
   React.useEffect(() => {
     if (!enable) return;
 
-    const eventSource = Endpoint.subscribe(endpoint, (newData) => {
-      setDataState(({ data }) => updateDataState(data, newData));
-      setTimestamp(Date.now());
-    });
+    const eventSource = Endpoint.subscribe(endpoint, (newData) => setData(newData));
 
     return () => eventSource.close();
   }, [enable, endpoint]);
 
-  return {...dataState, timestamp};
+  const { data, timestamp } = requestState;
+  return { data, timestamp };
 };
 //===========================================================================
